732bd478f43f13b94b48fb697d346145f2222939
crazy-medic <vineethtv001@outlook.com>
2021-09-22 20:59:33 +0530

    Employee attendance

---
+++ EmployeeAttendance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EmployeeAttendance
+{
+    class Program
+    {
+        /// <summary>
+        /// Employee Attendance Program.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int EmpCheck = random.Next(0,2);
+
+            if (EmpCheck == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
