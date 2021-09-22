aa8391d000f8693dbd019004e39830e8e2f53dc6
crazy-medic <vineethtv001@outlook.com>
2021-09-22 21:12:48 +0530

    Employee montly wage program

--- EmployeeAttendance/Program.cs
+++ EmployeeAttendance/Program.cs
@@ -4,23 +4,44 @@ namespace EmployeeAttendance
 {
     class Program
     {
-        /// <summary>
-        /// Employee Attendance Program.
-        /// </summary>
-        /// <param name="args">The arguments.</param>
+        // Declaring constants
+        public const int isFullTime = 1;
+        public const int isPartTime = 2;
+        public const int isAbsent = 0;
+        public const int EmpWagePerHr = 150;
+        public const int TotalWorkingDays = 20;
+
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int EmpCheck = random.Next(0,2);
+            // Declaring variables
+            int EmpWage = 0;
+            int day = 0;
+            int EmpHrs = 0;
+            int TotalEmpWage = 0;
 
-            if (EmpCheck == 1)
+            for (day = 0; day<20; day++)
             {
-                Console.WriteLine("Employee is Present");
+                // Creating random number
+                Random random = new Random();
+                int EmpCheck = random.Next(3);
+
+                if (EmpCheck == 1)
+                {
+                    EmpHrs = 8;
+                }
+                else if (EmpCheck == 2)
+                {
+                    EmpHrs = 5;
+                }
+                else
+                {
+                    EmpHrs = 0;
+                }
+                EmpWage = EmpWagePerHr * EmpHrs;
+                TotalEmpWage += EmpWage;
+                
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
+            Console.WriteLine("The total employee wage = " + TotalEmpWage);
         }
     }
 }
