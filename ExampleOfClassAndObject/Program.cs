1ba6023ddf4175fb7bee128a6990e3b58df1352a
crazy-medic <vineethtv001@outlook.com>
2021-09-24 20:14:59 +0530

    Pushing example program of class and object with constructors

---
+++ ExampleOfClassAndObject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExampleOfClassAndObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car obj = new Car();
+            Car obj1 = new Car("Alto");
+            Console.WriteLine(obj.color);
+            Console.WriteLine(obj1.NameOfCar);
+            obj.cardetails();
+        }
+    }
+
+    class Car
+    {
+        public string color = "red";
+        public string NameOfCar = "";
+        public Car(string name)
+        {
+            NameOfCar = name;
+        }
+
+        public Car()
+        { }
+        public void cardetails()
+        {
+            Console.WriteLine("Swift");
+        }
+        
+    }
+}
