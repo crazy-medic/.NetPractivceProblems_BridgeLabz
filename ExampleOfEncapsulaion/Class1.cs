3eb25b9307562012d21561d7dfb2c889dd344bd3
crazy-medic <vineethtv001@outlook.com>
2021-09-27 20:01:53 +0530

    Uploaded Encapsulation program

---
+++ ExampleOfEncapsulaion/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleOfEncapsulaion
+{
+    class Class1
+    {
+        private string Name;
+
+        public string NameOfPerson
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+    }
+}
---
+++ ExampleOfEncapsulaion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExampleOfEncapsulaion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.NameOfPerson = "TestingPerson";
+            Console.WriteLine(obj);
+        }
+    }
+}
