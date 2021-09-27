65036a7c57d1d84c37e5a557e445f30972d8df95
crazy-medic <vineethtv001@outlook.com>
2021-09-27 20:02:49 +0530

    Use case 1 of Employee wage program using objects

---
+++ EmployeeProgramWithObjects/Attendance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeProgramWithObjects
+{
+    class Attendance
+    {
+        public void PresentAbsent(int a)
+        {
+            if (a == 0)
+                Console.WriteLine("Employee is absent");
+            else
+                Console.WriteLine("Employee is present");
+        }
+    }
+}
---
+++ EmployeeProgramWithObjects/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeProgramWithObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Wage Computation Program");
+            Attendance att = new Attendance();
+            Random rand = new Random();
+            att.PresentAbsent(rand.Next(0,2));
+        }
+    }
+}
