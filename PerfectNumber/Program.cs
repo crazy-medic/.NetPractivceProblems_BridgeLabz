149ca25f6289d5a685591bb6935fad14ebb489c6
crazy-medic <vineethtv001@outlook.com>
2021-09-29 17:35:14 +0530

    Uploaded perfect number program

---
+++ PerfectNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Perfect Number Program!");
+            Console.WriteLine("Enter a number:");
+            int num = int.Parse(Console.ReadLine());
+            int i = 1;
+            int total = 0;
+
+            for (i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                    total += i;
+            }
+            if (total == num)
+                Console.WriteLine(num + " is a perfect number");
+            else
+                Console.WriteLine(num + " is not a perfect number");
+        }
+    }
+}
