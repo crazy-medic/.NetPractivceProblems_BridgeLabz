7de14dffca0774fda5864de9f45af78cc3ca38ec
crazy-medic <vineethtv001@outlook.com>
2021-09-29 12:51:41 +0530

    Uploaded Harmonic number program

---
+++ HarmonicNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Harmonic number program");
+            Console.WriteLine("Enter the nth number to print harmonic numbers:");
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i < n)
+            {
+                Console.Write("1/" + i+" + ");
+                i++;
+            }
+            Console.Write(value:"1/"+ i);
+        }
+    }
+}
