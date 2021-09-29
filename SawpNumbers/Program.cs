10b327450f430d99e29e1867efc41dd414a1fdf6
crazy-medic <vineethtv001@outlook.com>
2021-09-29 13:04:45 +0530

    Uploaded Swaping number program

---
+++ SawpNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SawpNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number swapping program");
+            Console.WriteLine("Enter 1st number:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number:");
+            int b = int.Parse(Console.ReadLine());
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After swapping:");
+            Console.WriteLine("1st number is " + a);
+            Console.WriteLine("2nd number is " + b);
+        }
+    }
+}
