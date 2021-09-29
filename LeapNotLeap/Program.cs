38b0164a02d9f98093cddf5561812723068caa17
crazy-medic <vineethtv001@outlook.com>
2021-09-29 11:58:18 +0530

    Uploaded Leap year program

---
+++ LeapNotLeap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeapNotLeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year to check:");
+            int year = int.Parse(Console.ReadLine());
+            if (year % 4 == 0 && year%100 != 0 || year%400 == 0)
+            {
+                Console.WriteLine("The given year is leap year!");
+            }
+            else
+            {
+                Console.WriteLine("The entered year is not leap!");
+            }
+        }
+    }
+}
