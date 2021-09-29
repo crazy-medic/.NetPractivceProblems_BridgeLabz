ef0d20c1974782f3591436a5722f3830d2b248d6
crazy-medic <vineethtv001@outlook.com>
2021-09-29 13:15:56 +0530

    Uploaded Char type checking for constant or vowel program

---
+++ ConstantOrVowel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConstantOrVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vowel Checker Program!");
+            Console.WriteLine("Enter a letter");
+            char Input = Convert.ToChar(Console.ReadLine());
+            if (Input == 'a' || Input == 'e' || Input == 'i' || Input == 'o' || Input == 'u' || Input == 'A' || Input == 'E' || Input == 'O' || Input == 'U' || Input == 'I')
+            {
+                Console.WriteLine("Entered charecter is vowel!");
+            }
+            else
+            {
+                Console.WriteLine("Entered charecter is constant!");
+            }
+        }
+    }
+}
