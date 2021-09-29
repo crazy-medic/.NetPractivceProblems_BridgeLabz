aabc593b19058bfbac9e06a9aa2957fc23ad7764
crazy-medic <vineethtv001@outlook.com>
2021-09-29 12:59:23 +0530

    Uploaded quotient and remainder program

---
+++ QuotientAndRemainder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace QuotientAndRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quotient and Remainder program.");
+            Console.WriteLine("Enter the bigger number to be divided:");
+            int Big = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the smaller number to be used for divide:");
+            int Small = int.Parse(Console.ReadLine());
+            int Quotient = Big / Small;
+            int Remainder = Big - (Quotient * Small);
+            Console.WriteLine("Qoutient of the 2 numbers are: " + Quotient);
+            Console.WriteLine("Remainder : " + Remainder);
+        }
+    }
+}
